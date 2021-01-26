9525bb8b5d664dabe73f6e9036f96cfab2e3cb84
eunjin0212 <eunjin9639@gmail.com>
2021-01-26 16:54:43 +0900

    Interfaces on Typescript

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface Human {
+    name: string,
+    age: number,
+    gender: string,
+}
+const person = {
+    name: 'Jin',
+    age: 28,
+    gender: 'female'
+}
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
+}
+
+console.log(sayHi(person));
+export { }; // 버그
