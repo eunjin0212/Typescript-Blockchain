1c1e579c4056abaf30f87d1d136b76f1baea19b8
eunjin0212 <eunjin9639@gmail.com>
2021-01-26 18:10:31 +0900

    Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ class Block {
 
     // sayHello() => return 'Hello' 블록을 생성했을때만 사용 가능한 메서드
 
-    static calculateBlockHash = (index: number, previousHash: string, timeStamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timeStamp + data).toString();
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(index: number, hash: string, previousHash: string, data: string, timeStamp: number) {
         this.index = index;
@@ -19,12 +19,23 @@ class Block {
         this.timeStamp = timeStamp;
     }
 }
+// validateStructure 
 
 const genesisBlock: Block = new Block(0, "224422414", '', 'Hello', 123456);
 let blockChain: Block[] = [genesisBlock];
 
-const getBlockChain = (): Block[] => blockChain;
+const getBlockchain = (): Block[] => blockChain;
 
 const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+}
+console.log(createNewBlock('Hello'), createNewBlock('bye'))
