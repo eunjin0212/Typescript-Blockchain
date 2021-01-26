0ffe8ebcaf97204a437ee758f7559e21d70f16b4
eunjin0212 <eunjin9639@gmail.com>
2021-01-26 16:25:01 +0900

    tsc-watch

--- index.ts
+++
@@ -1,8 +0,0 @@
-const name = 'Jin',
-    age = 28,
-    gender = 'female'
-const sayHi = (name, age, gender?) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
-}
-sayHi(name, age)
-export { }; // 버그
