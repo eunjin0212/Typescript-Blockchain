a93380732c5b7adc6c8cba78cc75cc40f5db23d3
eunjin0212 <eunjin9639@gmail.com>
2021-01-26 17:25:47 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string,
+import { throws } from "assert";
+
+class Human {
+    // js에 interface를 넣고 싶을때 class를 만든디.
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
-const person = {
-    name: 'Jin',
-    age: 28,
-    gender: 'female'
-}
+const lynn = new Human('Lynn', 18, 'female')
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
 }
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 export { }; // 버그
