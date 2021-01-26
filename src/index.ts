0051ecf52be8afc0364ba371bbe7d55e8d7a4c9a
eunjin0212 <eunjin9639@gmail.com>
2021-01-26 17:59:09 +0900

    Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import * as CryptoJS from 'crypto-js'
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timeStamp: number;
+
+    // sayHello() => return 'Hello' 블록을 생성했을때만 사용 가능한 메서드
+
+    static calculateBlockHash = (index: number, previousHash: string, timeStamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timeStamp + data).toString();
+
     constructor(index: number, hash: string, previousHash: string, data: string, timeStamp: number) {
         this.index = index;
         this.hash = hash;
@@ -13,7 +20,11 @@ class Block {
     }
 }
 
-const genesisBlock: Block = new Block(0, "224422414", '', 'Hello', 123456)
-let blockChain: [Block] = [genesisBlock];
+const genesisBlock: Block = new Block(0, "224422414", '', 'Hello', 123456);
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain)
+const getBlockChain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
